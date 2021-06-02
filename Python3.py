a979a7d6cb569284f49e6f92d31968f2c031f7b9
DigitalPresales <84257933+DigitalPresales@users.noreply.github.com>
2021-06-02 12:49:36 +0530

    Create Python3.py

---
+++ Python3.py
@@ -0,0 +1,7 @@
+
+# Define variables as shown below
+quantity = 15
+itemno = 500
+price = 69
+salesorder = "Sale {} of pieces of item number {} for {:.2f} dollars."
+print(salesorder.format(quantity, itemno, price))
