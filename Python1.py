7bfeee1fda84c89dd205feec3ea495dfbdbe702e
DigitalPresales <84257933+DigitalPresales@users.noreply.github.com>
2021-06-02 09:10:32 +0530

    Create Python1.py

---
+++ Python1.py
@@ -0,0 +1,11 @@
+
+import os
+def main(name):
+    print(f'Hi, {name}')
+    x, y = 5, 9
+
+    if (x < y):
+        print("%i is less than %i" %(x,y) )
+if __name__ == '__main__':
+    main('Digital PreSales')
+
