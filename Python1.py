786d76de476bceb1d1c3aeaaffd54d956e5bd7cd
DigitalPresales <84257933+DigitalPresales@users.noreply.github.com>
2021-06-08 13:16:02 +0530

    Update Python1.py

--- Python1.py
+++ Python1.py
@@ -6,6 +6,8 @@ def main(name):
 
     if (x < y):
         print("%i is less than %i" %(x,y) )
-if __name__ == '__main__':
+if __name__ == '__main__':    // is used to execute some code only if the file was run directly, and not imported.
     main('Digital PreSales')
-
+    
+# If you import this script as a module in another script, the __name__ is set to the name of the script/module.
+# So basically it either acts as a reusable modules, or as standalone programs. 
